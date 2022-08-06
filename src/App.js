256e7a99fe79210d6a0e6da9e55c98c90a887eeb
SUSIGUGH <susigugh@gmail.com>
2022-08-06 16:51:39 +0530

    Removed Left and Right to Middle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Middle from './components/Middle';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-main">
+      <div className="App-header">
+        <Header />        
+      </div>
+      <div className="App-body">
+        <div className="App-middle">
+          <Middle />
+        </div>
+      </div>
+      <div className="App-footer">
+        <Footer />
+      </div>
     </div>
+ 
   );
 }
 
