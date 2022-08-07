34cd7ee040e2f92cb9a865e6d20ee72973256c5c
SUSIGUGH <susigugh@gmail.com>
2022-08-07 11:44:05 +0530

    Added to list momo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import React from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import AddMomo from './components/AddMomo';
+import ListMomo from './components/ListMomo';
 
 
 function App() {
+  const momolist = [
+    {
+    id: "1",
+    momoname: "Chicken Steam",
+    momodescription: "Normal Steam Chicken Momo with Soup and Sauce"
+    },
+    {
+      id: "2",
+      momoname: "Chicken Fried",
+      momodescription: "Normal Deep Fried Chicken Momo with Sauce"
+      },
+  ]
   return (
     <div className="App-main">
       <div className="App-header">
@@ -14,6 +27,9 @@ function App() {
       <div className="App-body">
         <div className="App-middle">
           <AddMomo />
+        </div>
+        <div className="App-middle">
+          <ListMomo momolist={momolist}/>
         </div>
       </div>
       <div className="App-footer">
